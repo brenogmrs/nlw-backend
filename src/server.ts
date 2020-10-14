2922d8086bdf137bf87934eef874076d86fd0c5f
Breno Guimaraes <brenoaraujo@outlook.com.br>
2020-10-13 23:54:22 -0300

    feat: added and implemented in the controller the validation with yup

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import 'express-async-errors'
 import path from 'path';
 import './database/connection';
+import errorHandler from './errors/handler';
 import router from './routes' ;
 
 const app = express();
 app.use(express.json());
 app.use(router);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
-
+app.use(errorHandler)
 
 app.listen(3333);
