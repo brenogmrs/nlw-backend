e9bbb2959a6dddbfe2575f7cfdfd4fba916db93c
Breno Guimaraes <brenoaraujo@outlook.com.br>
2020-10-13 23:56:47 -0300

    feat: added and implementes cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import 'express-async-errors'
 import path from 'path';
+import cors from 'cors';
 import './database/connection';
 import errorHandler from './errors/handler';
 import router from './routes' ;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 app.use(router);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler)
